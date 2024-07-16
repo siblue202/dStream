766cb3d2779c86035d31f051c3cf2e34134c985f
JGH <siblue202@gmail.com>
2024-07-16 22:18:00 +0900

    remove local script

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#!/bin/bash
-
-cd spark-3.2.3 # clone한 폴더 이름에 맞게 변경
-sudo ./build/mvn -pl :spark-core_2.12,:spark-sql_2.12 -DskipTests clean install
-cp ./sql/core/target/spark-sql_2.12-3.2.3.jar ./assembly/target/scala-2.12/jars/
-cp ./core/target/spark-core_2.12-3.2.3.jar ./assembly/target/scala-2.12/jars/
-cd ..
--- propagate_buildfile.sh
+++
@@ -1,2 +0,0 @@
-scp spark-3.2.3/sql/core/target/spark-sql_2.12-3.2.3.jar lassjupiter0@163.239.199.220:/opt/spark/spark-3.2.3/assembly/target/scala-2.12/jars
-scp spark-3.2.3/core/target/spark-core_2.12-3.2.3.jar lassjupiter0@163.239.199.220:/opt/spark/spark-3.2.3/assembly/target/scala-2.12/jars
